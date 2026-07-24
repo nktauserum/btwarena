86a2fb4f23baae1231c79c541ec4e04628397fc9
nktauserum <auserum@proton.me>
2026-07-24 21:07:23 +0300

    Visual demonstration

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 #define BUFFER_SIZE 1024
 #include "pool.h"
@@ -12,13 +13,21 @@ void print_bitmap(Pool* pool) {
 }
 
 int main(void) {
-    Pool pool = {0};
+    Pool pool = {0}; // initialize the pool
     pool_init(&pool);
 
-    void* ptr = pool_get(&pool);
-    pool_put(&pool, ptr);
+    void* ptr = pool_get(&pool); // alloc
+
+    strcpy(ptr, "hello from the block 1");
+    printf("Wrote %lu bytes to %p: %s\n", strlen(ptr), ptr, (char*)ptr);
+
+    printf("Take a look how bitmap looks right now. All blocks are available except the first one: ");
+    print_bitmap(&pool);
+
+    pool_put(&pool, ptr); // free allocated block
 
     void* ptr1 = pool_get(&pool);
+    printf("Wonderful! On the second allocation we got a block with the same address: %p\n", ptr1);
     pool_put(&pool, ptr1);
 
     return 0;
