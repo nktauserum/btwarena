e9ba91b34acab2b8c2823fa4db28db3c182c6bb2
nktauserum <auserum@proton.me>
2026-07-28 18:06:50 +0300

    Fix incorrect naming: count instead of capacity

--- pool.h
+++ pool.h
@@ -8,17 +8,17 @@
 
 typedef struct {
     uint16_t bitmap;
-    size_t size, capacity; // capacity tells us how many buffers is in the pool, size - what size is each buffer 
+    size_t size, count; // count tells us how many buffers is in the pool, size - what size is each buffer 
     char* arena;
 } Pool;
 
-void pool_init(Pool* pool, size_t capacity, size_t size) {
-    pool->capacity = capacity < 8 ? 8 : capacity;
+void pool_init(Pool* pool, size_t count, size_t size) {
+    pool->count = count < 8 ? 8 : count;
     pool->size = size;
     
     pool->bitmap = ~0; // all fields are available
-    pool->arena = (char*)malloc(capacity*size);         
-    memset(pool->arena, 0, capacity * size);
+    pool->arena = (char*)malloc(count*size);         
+    memset(pool->arena, 0, count * size);
 }
 
 void* pool_get(Pool* pool) {
@@ -34,7 +34,7 @@ void* pool_get(Pool* pool) {
 
 void pool_put(Pool* pool, void* ptr) {
     int offset = ((char*)ptr - pool->arena) / pool->size; // a little **magic** of pointer arithmetic
-    if (offset < 0 || offset >= pool->capacity) return; // decline if ptr is possibly not within the arena
+    if (offset < 0 || offset >= pool->count) return; // decline if ptr is possibly not within the arena
 
     pool->bitmap |= (1u << offset); // set as true (always)
     memset(ptr, 0, pool->size); // clear given buffr
